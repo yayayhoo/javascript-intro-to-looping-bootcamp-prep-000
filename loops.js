37485fe46091765310523b86c6f12804d9f9435b
yesI <1317154549@qq.com>
2017-09-18 12:39:09 +0000

    Done.

---
+++ loops.js
@@ -0,0 +1,21 @@
+function forLoop(array) {
+
+
+  for (var i = 0; i < 25; i++) {
+
+    array[i] = "I am "+i+" strange loops.";
+  }
+    array[1] = "I am 1 strange loop.";
+  return array;
+}
+
+function whileLoop(n) {
+
+}
+
+function doWhileLoop(array) {
+  do {
+    array.pop();
+  }while (array.length > 0)
+  return array;
+}
