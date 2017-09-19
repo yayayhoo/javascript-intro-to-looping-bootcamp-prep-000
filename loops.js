32128cf26a648985486602c6cad19b8876e71fd9
yesI <1317154549@qq.com>
2017-09-19 00:30:06 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -19,6 +19,6 @@ function whileLoop(n) {
 function doWhileLoop(array) {
   do {
     array.pop();
-  }while (array.length or maybeTrue(array) == 'false')
+  }while (array.length>0 or maybeTrue(array) == 'false')
   return array.length;
 }
