8cacbf6d300c245d3dad580bd958d44178c49656
yesI <1317154549@qq.com>
2017-09-19 01:06:23 +0000

    Done.

--- loops.js
+++ loops.js
@@ -1,21 +1,29 @@
 function forLoop(array) {
+  var a=[];
+  n = 25;
+  for (var i = 0; i < n; i++) {
 
-
-  for (var i = 0; i < 25; i++) {
-
-    array[i] = "I am "+i+" strange loops.";
+    a[i] = "I am "+i+" strange loops.";
   }
-    array[1] = "I am 1 strange loop.";
-  return array;
+  a[1] = "I am 1 strange loop.";
+  return a.concat(array);
 }
 
 function whileLoop(n) {
-
+  while (n) {
+    // n--;
+    console.log(--n);
+  }
+  return 'done';
 }
 
 function doWhileLoop(array) {
   do {
     array.pop();
-  }while (array.length > 0)
+  }while (array.length>0 && maybeTrue() == 'false');
   return array;
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
 }
